aea937b3e9a189b500b967aeb94587a1bf6ea74e
sachikavindaa <sachikavindaa@gmail.com>
2020-11-18 22:58:10 +0530

    add new files

--- ML/Weather/Weather_2/weather_2.py
+++ ML/Weather/Weather_2/weather_2.py
@@ -26,10 +26,10 @@ from sklearn import preprocessing
 
 !git clone 'https://github.com/sachinkavindaa/Machine-learning-with-Python'
 
-df = pd.read_csv('/content/Machine-learning-with-Python/ML/Weather/Weather.csv')
+df = pd.read_csv('/content/Machine-learning-with-Python/ML/Weather/Weather_3/Weather.csv')
 df.head()
 
-merged = df[['T Max', 'T Min', 'Wind', 'Could','Precip','Humidity','Pressure']]
+merged = df[['T_Max', 'T_Min', 'Wind', 'Could','Precip','Humidity','Pressure']]
 merged.head()
 
 def min_max_scaling(merged):
@@ -49,7 +49,7 @@ df_scaled
 df_scaled.info()
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(df_scaled[['T Max', 'T Min', 'Wind', 'Could','Humidity','Pressure']],merged.Precip,test_size =0.2,random_state=25)
+X_train,X_test,y_train,y_test = train_test_split(df_scaled[['T_Max', 'T_Min', 'Wind', 'Could','Humidity','Pressure']],df_scaled.Precip,test_size =0.2,random_state=25)
 
 X_train
 
@@ -58,10 +58,10 @@ df_scaled.shape
 len(X_train)
 
 X_train_scaled = X_train.copy()
-X_train_scaled[['T Max', 'T Min', 'Wind', 'Could','Humidity','Pressure']]= X_train_scaled[['T Max', 'T Min', 'Wind', 'Could','Humidity','Pressure']]
+X_train_scaled[['T_Max', 'T_Min', 'Wind', 'Could','Humidity','Pressure']]= X_train_scaled[['T_Max', 'T_Min', 'Wind', 'Could','Humidity','Pressure']]
 
 X_test_scaled = X_test.copy()
-X_test_scaled[['T Max', 'T Min', 'Wind', 'Could','Humidity','Pressure']]= X_test_scaled[['T Max', 'T Min', 'Wind', 'Could','Humidity','Pressure']]
+X_test_scaled[['T_Max', 'T_Min', 'Wind', 'Could','Humidity','Pressure']]= X_test_scaled[['T_Max', 'T_Min', 'Wind', 'Could','Humidity','Pressure']]
 
 X_train_scaled
 
@@ -71,9 +71,11 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-model.fit(X_train_scaled,y_train,epochs=10000)
+model.fit(X_train_scaled,y_train,epochs=300)
 
 X_test_scaled
+
+model.evaluate(X_test_scaled,y_test)
 
 model.predict(X_test_scaled)
 
@@ -85,5 +87,19 @@ coef, intercept
 def sigmoid(x):
   import math
   return 1/(1+math.exp(-x))
-sigmoid(18)
+sigmoid(8)
 
+X_test
+
+def prediction_function(T_max,T_min,Wind,Could,Pressure,Humidity):
+  weighted_sum = coef[0] * T_max + coef[1] * T_min + coef[2] * Wind + coef[3] * Could + coef[4] * Pressure + coef[5] * Humidity + intercept
+  return sigmoid(weighted_sum)
+
+prediction_function(0.88, 0.008850 , 0.047619 , 0.170213 ,0.607143,	0.995619)
+
+"""When we input the variable values of the corresponding to the 364 raw, we can get value 0.40268136412389227. 
+
+
+
+model.predict(X_test_scaled) line gives the same value relative to 364 raw array in 0.40268135.
+"""
